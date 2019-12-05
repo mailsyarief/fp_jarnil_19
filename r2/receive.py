101ea49b1f55f0223995f498a33b35beebde8fc1
Hilmi Raditya <raditya113@icloud.com>
2019-12-05 20:43:23 +0700

    get location from all receiver

--- r2/receive.py
+++ r2/receive.py
@@ -1,29 +1,67 @@
+# -*- coding: utf-8 -*-
 import socket
 import struct
 import sys
+import pickle
 
-multicast_group = '224.3.29.71'
-server_address = ('', 10001)
+port = 10002
+lat_to = -7.265441
+long_to = 112.797662
 
-# Create the socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+def sendPosition():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.settimeout(20)
+    client.connect(('127.0.0.1', 35))
+    data = {
+        'port' : port,
+        'lat' : lat_to,
+        'long' : long_to
+    }
+    client.send(pickle.dumps(data))
+    print 'sukses mengirim lokasi !'
+    return client.close()
 
-# Bind to the server address
-sock.bind(server_address)
 
-# Tell the operating system to add the socket to the multicast group
-# on all interfaces.
-group = socket.inet_aton(multicast_group)
-mreq = struct.pack('4sL', group, socket.INADDR_ANY)
-sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+def multicast():
+    multicast_group = '224.3.29.71'
+    server_address = ('', port)
 
-# Receive/respond loop
-while True:
-    print >>sys.stderr, '\nwaiting to receive message'
-    data, address = sock.recvfrom(1024)
-    
-    print >>sys.stderr, 'received %s bytes from %s' % (len(data), address)
-    print >>sys.stderr, data
+    # Create the socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    print >>sys.stderr, 'sending acknowledgement to', address
-    sock.sendto('ack', address)
+    # Bind to the server address
+    sock.bind(server_address)
+
+    # Tell the operating system to add the socket to the multicast group
+    # on all interfaces.
+    group = socket.inet_aton(multicast_group)
+    mreq = struct.pack('4sL', group, socket.INADDR_ANY)
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+    # Receive/respond loop
+    while True:
+        print >>sys.stderr, '\nwaiting to receive message'
+        data, address = sock.recvfrom(1024)
+        
+        print >>sys.stderr, 'received %s bytes from %s' % (len(data), address)
+        print >>sys.stderr, data
+
+        print >>sys.stderr, 'sending acknowledgement to', address
+        sock.sendto('ack', address)
+
+if __name__ == '__main__':
+    print "receiver 1, port " + str(port) + ": "
+    print "=============="
+    while 1:
+        print "1. mengirimkan posisi ke sender"
+        print "2. receive data"
+        print "3. keluar"
+        inputan = raw_input('Pilihan > ')
+        if(inputan == '1'):
+            sendPosition()
+        elif(inputan == '2'):
+            multicast()
+        elif(inputan == '3'):
+            exit()
+        else :
+            print 'inputan salah'
--- s/send.py
+++ s/send.py
@@ -2,7 +2,40 @@
 import socket
 import struct
 import sys
+import os
+import json
+import pickle
+from geopy.distance import geodesic
 
+lat_from = -7.294080
+long_from = 112.801600
+port = list()
+
+def getLatLong():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    ip = "0.0.0.0"
+    port = 35
+    server.bind((ip, port))
+    server.listen(5)
+    print ('menunggu mendapatkan posisi receiver')
+    (client_socket, address) = server.accept()
+    data = pickle.loads(client_socket.recv(1024))
+    print ("========")
+    print ("mendapatkan titik lat long dari receiver port " + str(data['port']))
+    print ("isi data :")
+    print (data['lat'])
+    print (data['long'])
+    print ("========")
+    writeDistance(data['port'],getDistance(data['lat'],data['long']))
+    server.close()
+
+def sendDataInput():
+    message = raw_input("input pesan > ")
+    for p in port:
+        hasil = send(message, p)
+        while(hasil == 0):
+            hasil = send(message, p)
+    print ('pengiriman berhasil')
 
 def send(message,port):
     multicast_group = ('224.3.29.71', port)
@@ -11,27 +44,53 @@ def send(message,port):
     ttl = struct.pack('b', 1)
     sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
 
-    print 'mengirimkan pesan berisi : %s' % message
-    sock.sendto(message, multicast_group)
+    print ('mengirimkan pesan berisi : %s' % message)
+    pesan = {
+        'pesan' : message,
+        'urutan' : port
+    }
+    sock.sendto(pickle.dumps(pesan), multicast_group)   
 
     while True:
         try:
             data, server = sock.recvfrom(16)
         except:
-            print 'tidak ada respon dari port %s' % port
+            print ('tidak ada respon dari port %s' % port)
             sock.close()
             return 0
         else:
-            print 'menerima "%s" dari %s' % (data, server)
+            print ('menerima "%s" dari %s' % (data, server))
             sock.close()
             return 1
 
-if __name__ == '__main__':
-    port = [10000,10001,10002]
-    message = raw_input("input pesan > ")
-    for p in port:
-        hasil = send(message, p)
-        while(hasil == 0):
-            hasil = send(message, p)
 
-    print 'pengiriman berhasil'
+def getDistance(lat_to,long_to):
+    coords_1 = (lat_from, long_from)
+    coords_2 = (lat_to, long_to)
+    return geodesic(coords_1, coords_2).miles
+
+def writeDistance(port,distance):
+    file = open('log/'+str(port)+".txt","w") 
+    file.writelines(str(distance))
+    file.close()
+
+if __name__ == '__main__':
+    print ("sender multicast dtn")
+    while 1:
+        print ("1. mendapatkan semua lot lang dari semua receiver")
+        print ("2. mengurutkan urutan pengiriman ke receiver")
+        print ("3. menjalankan pengiriman data")
+        print ("4. keluar")
+        print ("5. lihat port receiver yang tersedia")
+        pilihan = raw_input("Pilihan > ")
+        if(pilihan == '1'):
+            getLatLong()
+        elif(pilihan == '2'):
+            print 'pilihan 2'
+        elif(pilihan == '3'):
+            print 'pilihan 3'
+        elif(pilihan == '4'):
+            exit()
+        elif(pilihan == '5'):
+            for x in port:
+                print(x)
